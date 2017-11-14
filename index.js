058d6bfb04f372880baf3b6d0795fdd8dcf3374c
Vlad Barosan <vladdb@microsoft.com>
2017-11-13 17:31:26 -0800

    Extract class for validation Worker

--- index.js
+++ index.js
@@ -16,160 +16,155 @@ const path = require('path'),
   uuidv4 = require('uuid/v4'),
   azure = require("azure-storage");
 
-masterHandler();
 
-function masterHandler() {
-  let workers = {};
+let workers = {};
 
-  let numWorkers = 1;
-  console.log(`Master cluster setting up ${numWorkers} workers...`);
+let numWorkers = 1;
+console.log(`Master cluster setting up ${numWorkers} workers...`);
 
-  // Check that workers are online
-  cluster.on('online', (worker) => {
-    console.log(`The worker ${worker.id} responded after it was forked`);
-  });
+// Check that workers are online
+cluster.on('online', (worker) => {
+  console.log(`The worker ${worker.id} responded after it was forked`);
+});
 
-  // restart workers if they die
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.id} finished ${signal || code}.`);
-    delete workers[worker.id];
-    console.log(JSON.stringify(Object.keys(workers)));
-  });
+// restart workers if they die
+cluster.on('exit', (worker, code, signal) => {
+  console.log(`worker ${worker.id} finished ${signal || code}.`);
+  delete workers[worker.id];
+  console.log(JSON.stringify(Object.keys(workers)));
+});
 
-  cluster.setupMaster({
-    exec: 'worker.js',
-    silent: false
-  });
+cluster.setupMaster({
+  exec: 'worker.js',
+  silent: false
+});
 
-  // Create default workers.
-  for (var i = 0; i < numWorkers; i++) {
-    const worker = cluster.fork();
-    workers[worker.id] = worker;
+// Create default worker
+const defaultWorker = cluster.fork();
+workers[defaultWorker.id] = defaultWorker;
+
+let start = Date.now();
+var swaggerSpecDevelopment = require('./openapi/oav-express.json');
+var swaggerSpecProduction = require('./openapi/oav-express-production.json');
+const ErrorCodes = oav.Constants.ErrorCodes;
+const port = process.env.PORT || 8080;
+const maxWorkers = 20;
+const app = express();
+var server;
+const resultsTable = "oavResults";
+const tableService = azure.createTableService();
+
+//App Insights instrumentation key is passed via APPINSIGHTS_INSTRUMENTATIONKEY env variable
+appInsights.setup()
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true)
+  .setUseDiskRetryCaching(true)
+  .start();
+
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to oav-express');
+});
+
+// serve swagger
+app.get('/swagger.json', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  let host;
+  if (server && server.address() && server.address().address) {
+    host = server.address().address;
+  }
+  if (host && (host === '::' || host === 'localhost')) {
+    res.send(swaggerSpecDevelopment);
+  } else {
+    res.send(swaggerSpecProduction);
+  }
+});
+
+// This responds a POST request for live validation
+app.post('/validate', (req, res) => {
+
+  for (const workerId in cluster.workers) {
+    cluster.workers[workerId].send(req.body);
   }
 
-  let start = Date.now();
-  var swaggerSpecDevelopment = require('./openapi/oav-express.json');
-  var swaggerSpecProduction = require('./openapi/oav-express-production.json');
-  const ErrorCodes = oav.Constants.ErrorCodes;
-  const port = process.env.PORT || 8080;
-  const maxWorkers = 20;
-  const app = express();
-  var server;
-  const resultsTable = "oavResults";
-  const tableService = azure.createTableService();
+  return res.status(200).send();
+});
 
-  //App Insights instrumentation key is passed via APPINSIGHTS_INSTRUMENTATIONKEY env variable
-  appInsights.setup()
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .start();
+// Create new set of models to validate against
+app.post('/validations', (req, res) => {
 
-  //view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug');
+  if (Object.keys(workers).length >= maxWorkers) {
+    return res.status(429).send({ error: "More live validations are running then the service currently supports. Try again later." });
+  }
 
-  app.use(bodyParser.json()); // for parsing application/json
-  app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-  app.use(express.static(path.join(__dirname, 'public')));
+  let durationInSeconds = Number.parseInt(req.body.duration);
 
-  app.get('/', (req, res) => {
-    res.send('Welcome to oav-express');
-  });
+  if (isNaN(durationInSeconds) || durationInSeconds > 60 * 60) {
+    return res.status(400).send({ error: "Duration is not a number or it is longer than maximum allowed value of 60 minutes." });
+  }
 
-  // serve swagger
-  app.get('/swagger.json', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    let host;
-    if (server && server.address() && server.address().address) {
-      host = server.address().address;
-    }
-    if (host && (host === '::' || host === 'localhost')) {
-      res.send(swaggerSpecDevelopment);
-    } else {
-      res.send(swaggerSpecProduction);
-    }
-  });
+  const validationId = uuidv4();
+  const workerEnv = {
+    repoUrl: req.body.repoUrl,
+    branch: req.body.branch,
+    resourceProvider: req.body.resourceProvider,
+    apiVersion: req.body.apiVersion,
+    duration: req.body.duration,
+    validationId: validationId,
+  };
+  const worker = cluster.fork(workerEnv);
+  workers[worker.id] = worker;
 
-  // This responds a POST request for live validation
-  app.post('/validate', (req, res) => {
+  return res.status(200).send({ "validationId": validationId });
+});
 
-    for (const workerId in cluster.workers) {
-      cluster.workers[workerId].send(req.body);
-    }
+// Get results for a specific validation model that was created
+app.get('/validations/:validationId', (req, res) => {
+  let validationId = req.params.validationId;
+  console.log(`got val ${validationId}`);
 
-    return res.status(200).send();
-  });
+  var query = new azure.TableQuery()
+    .where('PartitionKey eq ?', validationId);
 
-  // Create new set of models to validate against
-  app.post('/validations', (req, res) => {
+  tableService.queryEntities(resultsTable, query, null, function (error, result, response) {
+    if (!error) {
+      let entries = result.entries;
 
-    if (Object.keys(workers).length >= maxWorkers) {
-      return res.status(429).send({ error: "More live validations are running then the service currently supports. Try again later." });
-    }
+      // Result comes in the form  {Property: {_: value; $: type}}. Transform to {Property: Value}
+      let flatResponse = entries.map(entity => {
 
-    let durationInSeconds = Number.parseInt(req.body.duration);
-
-    if (isNaN(durationInSeconds) || durationInSeconds > 60 * 60) {
-      return res.status(400).send({ error: "Duration is not a number or it is longer than maximum allowed value of 60 minutes." });
-    }
-
-    const validationId = uuidv4();
-    const workerEnv = {
-      repoUrl: req.body.repoUrl,
-      branch: req.body.branch,
-      resourceProvider: req.body.resourceProvider,
-      apiVersion: req.body.apiVersion,
-      duration: req.body.duration,
-      validationId: validationId,
-    };
-    const worker = cluster.fork(workerEnv);
-    workers[worker.id] = worker;
-
-    return res.status(200).send({ "validationId": validationId });
-  });
-
-  // Get results for a specific validation model that was created
-  app.get('/validations/:validationId', (req, res) => {
-    let validationId = req.params.validationId;
-    console.log(`got val ${validationId}`);
-
-    var query = new azure.TableQuery()
-      .where('PartitionKey eq ?', validationId);
-
-    tableService.queryEntities(resultsTable, query, null, function (error, result, response) {
-      if (!error) {
-        let entries = result.entries;
-
-        // Result comes in the form  {Property: {_: value; $: type}}. Transform to {Property: Value}
-        let flatResponse = entries.map(entity => {
-
-          delete entity[".metadata"];
-          for (let [key, value] of Object.entries(entity)) {
-            if (value.hasOwnProperty('_')) {
-              entity[key] = value._;
-            }
+        delete entity[".metadata"];
+        for (let [key, value] of Object.entries(entity)) {
+          if (value.hasOwnProperty('_')) {
+            entity[key] = value._;
           }
-          return entity;
-        });
+        }
+        return entity;
+      });
 
-        return res.status(200).send(flatResponse);
-      } else {
-        return res.status(400).send({ error: error.message })
-      }
-    });
+      return res.status(200).send(flatResponse);
+    } else {
+      return res.status(400).send({ error: error.message })
+    }
   });
+});
 
-  server = app.listen(port, () => {
-    let host = server.address().address;
-    let port = server.address().port;
+server = app.listen(port, () => {
+  let host = server.address().address;
+  let port = server.address().port;
 
-    console.log(`oav - express app listening at http://${host}:${port}`);
-    return server;
-  });
-}
+  console.log(`oav - express app listening at http://${host}:${port}`);
+  return server;
+});
 
--- worker.js
+++ worker.js
@@ -13,51 +13,21 @@ const cluster = require('cluster'),
   os = require('os'),
   url = require('url'),
   uuidv4 = require('uuid/v4'),
-  azure = require("azure-storage");
+  azure = require("azure-storage"),
+  ValidationWorker = require("./lib/validationWorker");
 
-//App Insights instrumentation key is passed via APPINSIGHTS_INSTRUMENTATIONKEY env variable
-appInsights.setup()
-  .setAutoDependencyCorrelation(true)
-  .setAutoCollectRequests(true)
-  .setAutoCollectPerformance(true)
-  .setAutoCollectExceptions(true)
-  .setAutoCollectDependencies(true)
-  .setAutoCollectConsole(true)
-  .setUseDiskRetryCaching(true)
-  .start();
-
-
-const tableService = azure.createTableService();
-const resultsTable = "oavResults";
-tableService.createTableIfNotExists(resultsTable, function (error, result, response) {
-  if (!error) {
-  }
-});
-
-const operationValidationResults = {};
-
-let totalSuccessRequestCount = 0;
-let totalSuccessResponseCount = 0;
-let totalSuccessCount = 0;
-let totalOperationCount = 0;
-let validationId = process.env.validationId;
-
-console.log(`ValidationId is ${validationId}`)
+console.log(`ValidationId is ${process.env.validationId}`)
 
 const liveValidatorOptions = {
   git: {
     shouldClone: true,
-    url: 'https://github.com/vladbarosan/sample-openapi-specs',
+    url: 'https://github.com/vladbarosan/sample-openapi-specs'
   },
   directory: path.resolve(os.homedir(), `repo${cluster.worker.id}`)
 };
 
-if (process.env.validationId === undefined) {
-  validationId = uuidv4();
-}
-
 if (process.env.repoUrl !== undefined) {
-  console.log(`Repo url is: ${process.env.repoUrl}`)
+  console.log(`Repo url is: ${process.env.repoUrl}`);
   liveValidatorOptions.git.url = process.env.repoUrl;
 }
 
@@ -73,160 +43,8 @@ if (process.env.resourceProvider !== undefined && process.env.apiVersion) {
   liveValidatorOptions.swaggerPathsPattern = validationJsonsPattern;
 }
 
-const validator = new oav.LiveValidator(liveValidatorOptions);
+let durationInSeconds = Number.parseInt(process.env.duration);
+const validationWorker = new ValidationWorker(process.env.validationId, liveValidatorOptions,
+  durationInSeconds, process.env.resourceProvider, process.env.apiVersion, cluster);
 
-validator.initialize().then(() => {
-  console.log(`Live validator initialized for session ${validationId}`);
-
-
-  let durationInSeconds = Number.parseInt(process.env.duration);
-
-  console.log('setting timeout for worker to be  ' + process.env.duration + 'parsed is ' + durationInSeconds);
-
-  if (!isNaN(durationInSeconds)) {
-    console.log('setting timeout for worker to be  ' + durationInSeconds);
-    setTimeout(() => {
-      uploadValidationResults();
-      console.log("getting ready to disconnect");
-      appInsights.defaultClient.flush();
-      cluster.worker.disconnect();
-      setTimeout(() => {
-        cluster.worker.kill();
-      }, 2000);
-
-    }, 1000 * durationInSeconds);
-  }
-});
-
-function getProvider(path) {
-  if (path === null || path === undefined || typeof path.valueOf() !== 'string' || !path.trim().length) {
-    throw new Error('path is a required parameter of type string and it cannot be an empty string.');
-  }
-
-  let providerRegEx = new RegExp('/providers/(\:?[^{/]+)', 'gi');
-  let result;
-  let pathMatch;
-
-  while ((pathMatch = providerRegEx.exec(path)) != null) {
-    result = pathMatch[1];
-  }
-
-  return result;
-}
-
-function validateHandler(requestBody) {
-
-  let parsedUrl = url.parse(requestBody.liveRequest.url, true);
-  let path = parsedUrl.pathname;
-
-  // Lower all the keys of query parameters before searching for `api-version`
-  console.log(`parsed url is ${JSON.stringify(parsedUrl.query)}`);
-
-  let apiVersion = parsedUrl.query['api-version'];
-  let resourceProvider = getProvider(path);
-
-  if (resourceProvider !== process.env.resourceProvider) {
-    console.log(`received request  is ${resourceProvider} is different from worker resource Provider ${process.env.resourceProvider} `);
-    return;
-  }
-
-  if (apiVersion !== process.env.apiVersion) {
-    console.log(`received request api version is ${apiVersion} is different from worker resource apiVersion ${process.env.apiVersion} `);
-    return;
-  }
-
-  let validationResult = validator.validateLiveRequestResponse(requestBody);
-
-  if (process.env.resourceProvider !== undefined) {
-    updateStats(validationResult);
-  }
-  console.log(JSON.stringify(validationResult));
-}
-
-function uploadValidationResults() {
-  console.log(JSON.stringify(operationValidationResults));
-
-  const entGen = azure.TableUtilities.entityGenerator;
-
-  const resultsEntity = {
-    PartitionKey: entGen.String(validationId),
-    RowKey: entGen.String("total"),
-    ResourceProvider: entGen.String(process.env.resourceProvider),
-    ApiVersion: entGen.String(process.env.apiVersion),
-    ModelSourceRepo: entGen.String(process.env.repoUrl),
-    ModelSourceBranch: entGen.String(process.env.branch),
-    Operations: entGen.Int32(totalOperationCount),
-    SuccessOperations: entGen.Int32(totalSuccessCount),
-    SuccessRate: entGen.Double((100 * (totalSuccessCount / totalOperationCount)).toPrecision(3)),
-    SuccessRequests: entGen.Int32(totalSuccessRequestCount),
-    SuccessResponses: entGen.Int32(totalSuccessResponseCount)
-  };
-
-  tableService.insertEntity(resultsTable, resultsEntity, function (error, result, response) {
-    if (!error) {
-      // result contains the ETag for the new entity
-    }
-  });
-
-  for (const [operationId, operationResults] of Object.entries(operationValidationResults)) {
-    const resultsEntity = {
-      PartitionKey: entGen.String(validationId),
-      RowKey: entGen.String(operationId),
-      ResourceProvider: entGen.String(process.env.resourceProvider),
-      ApiVersion: entGen.String(process.env.apiVersion),
-      ModelSourceRepo: entGen.String(process.env.repoUrl),
-      ModelSourceBranch: entGen.String(process.env.branch),
-      Operations: entGen.Int32(operationResults.operationCount),
-      SuccessOperations: entGen.Int32(operationResults.successCount),
-      SuccessRate: entGen.Double((100 * (operationResults.successCount / operationResults.operationCount)).toPrecision(3)),
-      SuccessRequests: entGen.Int32(operationResults.successRequestCount),
-      SuccessResponses: entGen.Int32(operationResults.successResponseCount)
-    };
-
-    tableService.insertEntity(resultsTable, resultsEntity, function (error, result, response) {
-      if (!error) {
-        // result contains the ETag for the new entity
-      }
-    });
-  }
-}
-
-function updateStats(validationResult) {
-
-  let operationId = validationResult.requestValidationResult.operationInfo[0].operationId;
-  let logSeverity = 3;
-  ++totalOperationCount;
-
-  if (!operationValidationResults[operationId]) {
-    operationValidationResults[operationId] = {
-      operationCount: 0,
-      successCount: 0,
-      successRequestCount: 0,
-      successResponseCount: 0
-    }
-  }
-
-  ++operationValidationResults[operationId].operationCount;
-
-  if (validationResult.requestValidationResult.successfulRequest === true) {
-    ++totalSuccessRequestCount;
-    ++operationValidationResults[operationId].successRequestCount;
-  }
-
-  if (validationResult.responseValidationResult.successfulResponse === true) {
-    ++totalSuccessResponseCount;
-    ++operationValidationResults[operationId].successResponseCount;
-  }
-
-  const isOperationSuccessful = validationResult.requestValidationResult.successfulRequest && validationResult.responseValidationResult.successfulResponse;
-
-  if (isOperationSuccessful) {
-    ++totalSuccessCount;
-    ++operationValidationResults[operationId].successCount;
-    logSeverity = 4;
-  }
-
-  appInsights.defaultClient.trackTrace({ message: JSON.stringify(validationResult), properties: { "validationId": validationId, "operationId": operationId, "isSuccess": isOperationSuccessful }, severity: logSeverity });
-}
-
-cluster.worker.on("message", validateHandler);
+validationWorker.start();
